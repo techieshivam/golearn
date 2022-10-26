1ec6edfe5a008ec6ccfd2b0ae4cbb4d9b9bf4b11
techieshivam <shivam.kb91@gmail.com>
2022-10-26 11:25:55 +0530

    sum of squares is completed

---
+++ forloop/9.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+) 
+	
+func main(){
+	var n,i  int 
+	fmt.Println("square of first N ")
+	fmt.Scanln(&n)
+	sum:=0
+	square:=0
+
+	for i=1;i<=n;i++ {
+	 square =i*i
+	
+	 sum=sum+square
+
+	
+	}
+	fmt.Println("sum of square is",sum) 
+
+}
