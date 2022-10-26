f01501288b6c6aefb2faedac214f47b74eebcc67
techieshivam <shivam.kb91@gmail.com>
2022-10-26 16:27:21 +0530

    vertical line inverted

---
+++ forloop/16.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	for i:=1;i<=5;i++{
+		for j:=1;j<=5;j++{
+			if i+j==6{
+			fmt.Print("*")
+	}else{
+		fmt.Print(" ")
+	}
+		}
+		fmt.Println()
+	}
+}
