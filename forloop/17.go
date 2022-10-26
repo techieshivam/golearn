c9cfccd2295f17b2157c27d4374b0a5de9780174
techieshivam <shivam.kb91@gmail.com>
2022-10-26 17:04:14 +0530

    equiletral triangle done

---
+++ forloop/17.go
@@ -0,0 +1,16 @@
+package main
+import (
+	"fmt"
+)
+func main(){
+	for i:=5;i>0;i--{
+		for j:=0;j<=5;j++{
+			if j<i{
+			fmt.Print(" ")
+		}else{
+			fmt.Print("* ")
+		}
+		}
+		fmt.Println()
+	}
+}
