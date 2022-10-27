92c8600b81492d3ed716abc7b94f6e6c2bc46970
techieshivam <shivam.kb91@gmail.com>
2022-10-27 14:41:21 +0530

    reverse the no

---
+++ forloop/30.go
@@ -0,0 +1,16 @@
+package main  
+import(
+	"fmt"
+)
+func main(){
+	var n,r int 
+	fmt.Println("enter your no.")
+	fmt.Scanln(&n)
+sum:=0
+for n!=0 {
+	r=n%10
+	sum=sum*10+r
+	n=n/10
+}
+fmt.Println(sum)
+}
