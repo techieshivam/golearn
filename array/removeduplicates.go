9662b43814c04d5e97b97f7ec3b767bf659dd35c
techieshivam <shivam.kb91@gmail.com>
2022-11-05 17:29:56 +0530

    unique element in array

---
+++ array/removeduplicates.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var size int
+	fmt.Println("enter the size of array")
+	fmt.Scanln(&size)
+
+	if size == 0 {
+		fmt.Println("No action can be performed!")
+		return
+	}
+
+	array := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		fmt.Printf("Enter value of array[%d] = ", i)
+		fmt.Scanln(&array[i])
+	}
+
+	fmt.Println("Array is :", array)
+	var result = make(map[int]int)
+
+	for _, num := range array {
+
+		result[num] = result[num] + 1
+		if result[num] == 1 {
+			fmt.Println("unique element in array is", num)
+		}
+	}
+
+}
