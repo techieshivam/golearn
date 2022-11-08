311cf4a2d25de6c40afc46791a2e919d8dd575b0
techieshivam <shivam.kb91@gmail.com>
2022-11-08 10:34:47 +0530

    lines fixed

--- array/deletelement.go
+++ array/deletelement.go
@@ -14,24 +14,20 @@ func main() {
 		return
 	}
 
-	array:=make([]int,size)
-	
-	for i:=0;i<size;i++{
+	array := make([]int, size)
+
+	for i := 0; i < size; i++ {
 		fmt.Printf("Enter value of array[%d] = ", i)
 		fmt.Scanln(&array[i])
-		}
+	}
 
-	fmt.Println("Array is :",array)
+	fmt.Println("Array is :", array)
 
 	fmt.Println("which element you want to delete")
 	var position int
 	fmt.Scanln(&position)
 
-	array=append(array[: position],array[position+1:]...)
-
-	fmt.Println("New Array is :",array)
-
-
-
+	array = append(array[:position], array[position+1:]...)
+	fmt.Println("New Array is :", array)
 
 }
